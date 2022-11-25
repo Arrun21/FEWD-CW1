a5922f1ed32adf8a9449dccd167ff247188b9703
Arrun21 <achoi200@caledonian.ac.uk>
2022-11-25 16:29:29 +0000

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import DisplayFoodItems from "./components/DisplayFoodItems"
 import { items } from "./data/data";
+import Search from "./components/Search"
 
 console.log( {items})
 function App() {
   return (
     <div>
       <h1>Recipes</h1>
-      <DisplayFoodItems foodList={items} />
+      {/* <DisplayFoodItems foodList={items} /> */}
+      <Search details={items}/>
     </div>
   );
 }
