0a670734ef1b1047eb94827c9ae271c161d32dcf
Arrun21 <achoi200@caledonian.ac.uk>
2022-12-07 18:26:34 +0000

    add review textbox

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Accordion } from "react-bootstrap";
+import { items } from '../data/data';
 import Star from "./star";
 import Stars from "./stars";
 
@@ -7,21 +8,32 @@ import Stars from "./stars";
 
 const Review = (params) => {
     const[selectedStars,setSelectedStars] = useState(0)
+    const[reviewText,setReviewText] = useState("")
     const handleClick = ()=>{
         if(selectedStars!=0){
-            params.food.review.push(selectedStars)
+          if(reviewText!=""){
+            const newReview = {
+              stars:selectedStars,
+              text:reviewText,
+            }
+            params.food.review.push(newReview)
             console.log(params.food.review)
             setSelectedStars(0);
+            setReviewText("");
+          }
         }
     }
     const createArray = length => [...Array(length)];
-    const average = params.food?.review?.reduce((total,next) => total+next,0)/params.food?.review?.length
+    const average = params.food?.review?.reduce((total,next) => total+next.stars,0)/params.food?.review?.length
     const totalStars = 5
   return (
     <Accordion.Body className="accordion-color">
-      <p id="review-heading">Review</p>
+      <p className="review-heading">Review</p>
     <div>
         {params.food?.review?.length > 0 && <div><p id="averagerating">Average Ratings</p><Stars average={average}/></div>}
+        {params.food.review.length > 0 && <div><br></br><p className="review-heading">All Reviews</p>{params.food.review.map((item)=>{
+          return <><Stars average={item.stars}/> <p>{item.text}</p></>
+        })}</div>}
         <div>
         <p id="selectrating">Select rating</p>
       {createArray(totalStars).map((n, i) => (
@@ -37,8 +49,9 @@ const Review = (params) => {
       <p>
         {selectedStars} of {totalStars} stars
       </p>
+      <textarea value={reviewText} onChange={(e)=>setReviewText(e.target.value)}/>
     </div>
-    <button className="button-style" onClick={()=>handleClick()} disabled={selectedStars === 0}>Add Review</button>
+    <button className="button-style" onClick={()=>handleClick()} disabled={selectedStars === 0 || reviewText === ""}>Add Review</button>
     </div>
     </Accordion.Body>
   )
